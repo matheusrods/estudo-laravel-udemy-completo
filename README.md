fb8432df9a7bf0dbbf716a68071af0a1cb6df6fb
tatiane nunes <tatyane0309@hotmail.com>
2023-01-16 15:14:31 -0300

    estudo laravel completo

---
+++ README.md
@@ -0,0 +1,1 @@
+echo "# curso-laravel-udemy";
---
+++ app_super_gestao/routes/web.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', 'PrincipalController@principal')->name('site.index');
+    
+Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.sobrenos');
+    
+Route::get('/contato', 'ContatoController@contato')->name('site.contato');
+
+Route::get('/login', function(){ return 'Login'; })->name('site.login');
+
+Route::prefix('/app')->group(function(){
+    Route::get('/clientes', function(){ return 'Clientes'; })->name('app.clientes');
+    Route::get('/fornecedores', function(){ return 'Fornecedores'; })->name('app.fornecedores');
+    Route::get('/produtos', function(){ return 'Produtos'; })->name('app.produtos');
+});
+
+
+
+//nome, categoria, assunto, mensagem
+
+// Route::get(
+//     '/contato/{nome?}/{categoria?}/{assunto?}/{mensagem?}', 
+//     function(
+//         string $nome = 'Desconhecido', 
+//         string $categoria = 'Informação', 
+//         string $assunto = 'Contato', 
+//         string $mensagem = 'mensagem nao informada'){
+//         echo 'estamos aqui: '.$nome.' - '.$categoria.' - '.$assunto.' - '.$mensagem;
+//     }
+// );
+// Route::get(
+//     '/contato/{nome}/{categoria_id}', 
+//     function(
+//         string $nome = 'Desconhecido', 
+//         int $categoria_id = 1){
+//         echo 'estamos aqui: '.$nome.' - '.$categoria_id;
+//     }
+// )->where('categoria_id','[0-9]+')->where('nome', '[A-Z-z]+');
