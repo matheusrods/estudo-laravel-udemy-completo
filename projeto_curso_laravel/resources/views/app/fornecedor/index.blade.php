4061476a24dd5a0ab59b0a77f358dac94aea83f5
Matheus Rodrigues <matheus.wilson.ext@ithealth.com.br>
2023-01-23 11:11:47 -0300

    incluindo comentarios e blocos PHP puros

--- projeto_curso_laravel/resources/views/app/fornecedor/index.blade.php
+++ projeto_curso_laravel/resources/views/app/fornecedor/index.blade.php
@@ -1,3 +1,13 @@
 <h3>Fornecedor</h3>
 
 {{-- Fica o comentario que será descartado pelo interpretador do blade --}}
+
+@php
+    // para comentarios de uma linha
+
+    /*
+        Para comentarios de multiplas linhas
+    */
+
+    echo 'Texto de teste';
+@endphp
