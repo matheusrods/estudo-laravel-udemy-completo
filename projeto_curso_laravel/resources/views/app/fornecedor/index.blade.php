731a270da9e296315040b2ce33f7b2c10e4446de
Matheus Rodrigues <matheus.wilson.ext@ithealth.com.br>
2023-01-23 11:27:37 -0300

    blade - @if/@else

--- projeto_curso_laravel/resources/views/app/fornecedor/index.blade.php
+++ projeto_curso_laravel/resources/views/app/fornecedor/index.blade.php
@@ -1,13 +1,9 @@
 <h3>Fornecedor</h3>
 
-{{-- Fica o comentario que será descartado pelo interpretador do blade --}}
-
-@php
-    // para comentarios de uma linha
-
-    /*
-        Para comentarios de multiplas linhas
-    */
-
-    echo 'Texto de teste';
-@endphp
+@if (count($fornecedores) > 0 && count($fornecedores) < 10)
+    <h3>Existem alguns fornecedores cadastrados</h3>
+@elseif(count($fornecedores) > 10)
+    <h3>Existem varios fornecedores cadastrados</h3>
+@else
+    <h3>Ainda nao existem fornecedores cadastrados</h3>
+@endif
